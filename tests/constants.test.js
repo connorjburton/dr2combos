fe7edfa86d3c2712ee9d48629a04f9aa78401416
James Salloway <james@jmsalloway.co.uk>
2020-06-11 23:57:53 +0100

    Update constants test to pass

--- tests/constants.test.js
+++ tests/constants.test.js
@@ -1,19 +1,21 @@
 import Constants from '../src/constants.js';
 
-describe('Items', () => {
-	test('Should not contain items that are not part of combos', () => {
-		const uniqueComboItems = Constants.COMBO_CARDS.reduce((aggregate, card) => {
-			card.weapons.forEach((weapon) => {
-				if (!aggregate.includes(weapon)) {
-					aggregate.push(weapon);
-				}
+describe('Constants', () => {
+	describe('ITEMS', () => {
+		test('Should not contain items that are not part of combos', () => {
+			const uniqueComboItems = Constants.COMBO_CARDS.reduce((aggregate, card) => {
+				card.items.forEach((item) => {
+					if (!aggregate.includes(item)) {
+						aggregate.push(item);
+					}
 
-				return;
-			});
+					return;
+				});
 
-			return aggregate;
-		}, []);
+				return aggregate;
+			}, []);
 
-		expect(Constants.ITEMS.sort()).toEqual(uniqueComboItems.sort());
+			expect(Constants.ITEMS.sort()).toEqual(uniqueComboItems.sort());
+		});
 	});
 });
